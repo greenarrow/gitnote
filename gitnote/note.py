ae995771d228bb1a8f5948bb651ddda2305a6a7b
Stefan Blanke <greenarrow@users.sourceforge.net>
2012-04-18 19:40:54 +0100

    gitnote/note: Flesh out basic methods
    Implement basic methods for loading and saving notes.

--- gitnote/note.py
+++ gitnote/note.py
@@ -1,3 +1,23 @@
+import os
+import uuid
+
 class Note(object):
-    def __init___(self):
-        pass
+    html = ""
+    title = "New Note"
+
+    def __init__(self, filename):
+        self.filename = filename
+
+        if os.path.exists(filename):
+            self.html = open(filename).read()
+
+    @staticmethod
+    def create():
+        return Note(os.path.join(os.path.expanduser("~"), ".gitnote",
+                    "%s.note" % uuid.uuid4()))
+    def save(self):
+       open(self.filename, "w").write(self.html)
+
+    def get_title(self):
+        return self.title
+	
