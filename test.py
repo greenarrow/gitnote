093c76bd9c97cd185fcdb8ae98414787c04ee99a
Stefan Blanke <greenarrow@users.sourceforge.net>
2012-04-18 21:04:35 +0100

    test: For testing accept note file as argument
    Opens up the specified file, otherwise creates a new one.

--- test.py
+++ test.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 
+import sys
 import gtk
 
 from gitnote.editor import Editor
 from gitnote.note import Note
 
 if __name__ == "__main__":
-    n = Note()
+    if len(sys.argv) > 1:
+        n = Note(sys.argv[1])
+    else:
+        n = Note.create()
+
     e = Editor(n)
     e.show_all()
     gtk.main()
+
