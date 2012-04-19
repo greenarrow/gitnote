b6f005c5b8329214f85d8c4db3d8ef3f20b9197f
Stefan Blanke <greenarrow@users.sourceforge.net>
2012-04-20 00:42:39 +0100

    gitnote/catalogue: Sort notes by mtime upon load
    Now they will be displayed most recently modified first.

--- gitnote/catalogue.py
+++ gitnote/catalogue.py
@@ -6,6 +6,7 @@ import note
 class Catalogue(object):
     def __init__(self, path):
         self.notes = map(note.MetaNote, glob.glob(os.path.join(path, "*.note")))
+        self.notes.sort(key=lambda n: n.get_mtime(), reverse=True)
 
     def get_notes(self):
         return self.notes
