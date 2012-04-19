8858b4ce81c5c4509537c5247e0e77ee0dfcc1f9
Stefan Blanke <greenarrow@users.sourceforge.net>
2012-04-19 17:03:59 +0100

    test: Expand test script to take commands including catalogue
    Adds simple demonstration of loading catalogue of notes.

--- test.py
+++ test.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python
 
+import os
 import sys
+import glob
 import gtk
 
 from gitnote.editor import Editor
 from gitnote.note import Note
 
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        n = Note(sys.argv[1])
-    else:
-        n = Note.create()
 
-    e = Editor(n)
-    e.show_all()
+PATH_NOTES = os.path.join(os.path.expanduser("~"), ".gitnote", "notes")
+
+
+if __name__ == "__main__":
+    assert len(sys.argv) > 1
+
+    if sys.argv[1] == "catalogue":
+        for filename in glob.glob(os.path.join(PATH_NOTES, "*.note")):
+            print Note(filename).get_title()
+
+        sys.exit(0)
+
+    elif sys.argv[1] == "new":
+        n = Note.create()
+        e = Editor(n)
+        e.show_all()
+
+    elif sys.argv[1] == "load":
+        for arg in sys.argv[2:]:
+            n = Note(arg)
+            e = Editor(n)
+            e.show_all()
+
     gtk.main()
 
