b8bb2a4b1d459b03c686c6b2833c4269dfad27c6
Stefan Blanke <greenarrow@users.sourceforge.net>
2012-04-28 08:59:43 +0100

    gitnote/git: Implement diff() method
    Returns the text of the diff for a given file.

--- gitnote/git.py
+++ gitnote/git.py
@@ -10,23 +10,28 @@ class Repository(object):
     def __init__(self, path):
         self.path = path
 
-    def _shell(self, args):
+    def _shell(self, args, stdout=None):
         prev = os.getcwd()
         os.chdir(self.path)
 
         try:
-            p = subprocess.Popen(["git"] + list(args))
+            p = subprocess.Popen(["git"] + list(args), stdout=stdout)
             if p.wait() != 0:
                 raise GitError
 
         finally:
             os.chdir(prev)
 
+        return p.communicate()
+
     def exists(self):
         return os.path.isdir(os.path.join(self.path, ".git"))
 
     def init(self):
         self._shell(["init"])
+
+    def diff(self, filename):
+        return self._shell(["diff", filename], stdout=subprocess.PIPE)[0]
 
     def add(self, filename):
         self._shell(["add", filename])
